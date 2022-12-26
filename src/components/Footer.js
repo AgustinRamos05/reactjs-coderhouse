bc12cef2793864d652202303d08b4c4b79f4f70d
Agustin Ramos <agustinramos0504@gmail.com>
2022-12-26 15:47:58 -0300

    Agregado de img al main y tipografia al proyecto

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Nav from "./Navbar";
 
+const Facebook = "https://www.facebook.com/"
+const Twitter = "https://twitter.com/?lang=es"
+const Instagram = "https://www.instagram.com/"
+
 const Footer = () => {
   return (
     <footer className="footer">
       <Nav 
       isHeader={false}
-      hrefFacebook = "https://www.facebook.com/"
-      hrefTwitter = "https://twitter.com/?lang=es"
-      hrefInstagram = "https://www.instagram.com/"
+      hrefFacebook = {Facebook}
+      hrefTwitter = {Twitter}
+      hrefInstagram = {Instagram}
        />
       <p className="footer__copyright">Copyright &copy; 2022</p>
     </footer>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ const Main = () => {
   return (
     <main className='main'>
       <h1 className='main__title'>Welcome to IponeShop</h1>
+      <img src="/img/592932.jpg" className='main__banner' alt="Banner Iphone 13"/>
     </main>
   )
 }
